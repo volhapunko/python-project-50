ffc3d3420e5de0172c14d77aa0a16fa898ea0e22
Volha <olgapunko1996@gmail.com>
2026-03-04 20:54:34 +0300

    move logic to cli.py

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,14 +1,4 @@
-import argparse
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.')
-    
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-
-    args = parser.parse_args()
+from gendiff.cli import main
 
 
 if __name__ == "__main__":
