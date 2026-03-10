7bfce5842fc72a9bbfa17468d49226e597e2e1b2
Volha <olgapunko1996@gmail.com>
2026-03-10 21:33:39 +0300

    add generate_diff function

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,2 +1,20 @@
-def generate_diff():
-    
+def generate_diff(data1, data2):
+    result = []
+    result.append('{')
+
+    all_keys = sorted(set(data1.keys() | data2.keys()))
+    for key in all_keys:
+        if key not in data2:
+            result.append(f'  - {key}: {data1[key]}')
+        elif key not in data1:
+            result.append(f'  + {key}: {data2[key]}')
+        elif data1[key] == data2[key]:
+            result.append(f'    {key}: {data1[key]}')
+        else:
+            result.append(f'  - {key}: {data1[key]}')
+            result.append(f'  + {key}: {data2[key]}')
+        
+    result.append('}')
+
+    result_str = '\n'.join(result)
+    return result_str
