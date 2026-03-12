0a4ab90c623dc922e3bac0851d6c1276761037a5
Volha <olgapunko1996@gmail.com>
2026-03-12 23:17:44 +0300

    fix diff.py

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -1,3 +1,9 @@
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return str(value)
+
+
 def generate_diff(data1, data2):
     result = []
     result.append('{')
@@ -5,14 +11,14 @@ def generate_diff(data1, data2):
     all_keys = sorted(set(data1.keys() | data2.keys()))
     for key in all_keys:
         if key not in data2:
-            result.append(f'  - {key}: {data1[key]}')
+            result.append(f'  - {key}: {format_value(data1[key])}')
         elif key not in data1:
-            result.append(f'  + {key}: {data2[key]}')
+            result.append(f'  + {key}: {format_value(data2[key])}')
         elif data1[key] == data2[key]:
-            result.append(f'    {key}: {data1[key]}')
+            result.append(f'    {key}: {format_value(data1[key])}')
         else:
-            result.append(f'  - {key}: {data1[key]}')
-            result.append(f'  + {key}: {data2[key]}')
+            result.append(f'  - {key}: {format_value(data1[key])}')
+            result.append(f'  + {key}: {format_value(data2[key])}')
         
     result.append('}')
 
