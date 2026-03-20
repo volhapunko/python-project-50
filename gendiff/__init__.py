c339e03b2f38e8205609ec440bb3e4317d780688
Volha <olgapunko1996@gmail.com>
2026-03-20 18:51:49 +0300

    add default formatter stylish for the library

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,8 @@
-from .diff import generate_diff
+from gendiff.builder.build_diff import build_diff
+from gendiff.formatter.stylish import format_stylish
+
+def generate_diff(data1, data2, format_name='stylish'):
+    diff = build_diff(data1, data2)
+
+    if format_name == 'stylish':
+        return format_stylish(diff)
