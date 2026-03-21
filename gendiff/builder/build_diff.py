b36cd2aea3d623e9677fc4716441e1e2f131832d
Volha <olgapunko1996@gmail.com>
2026-03-21 20:15:08 +0300

    refactor build_diff.py

--- gendiff/builder/build_diff.py
+++ gendiff/builder/build_diff.py
@@ -1,17 +1,37 @@
 def build_diff(data1, data2):
-    diff = {}
+    diff = []
     all_keys = sorted(set(data1.keys() | data2.keys()))
 
     for key in all_keys:
-        if key in data1 and key not in data2:
-            diff[key] = ('deleted', data1[key])
-        elif key in data2 and key not in data1:
-            diff[key] = ('added', data2[key])
+        if key not in data2:
+            diff.append({
+                'key': key,
+                'status': 'removed',
+                'value': data1[key]
+            })
+        elif key not in data1:
+            diff.append({
+                'key': key,
+                'status': 'added',
+                'value': data2[key]
+            })
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            diff[key] = ('nested', build_diff(data1[key], data2[key]))
+            diff.append({
+                'key': key,
+                'status': 'nested',
+                'children': build_diff(data1[key], data2[key])
+            })
         elif data1[key] == data2[key]:
-            diff[key] = ('unchanged', data1[key])
+            diff.append({
+                'key': key,
+                'status': 'unchanged',
+                'value': data1[key]
+            })
         else:
-            diff[key] = ('changed', data1[key], data2[key])
-        
+            diff.append({
+                'key': key,
+                'status': 'changed',
+                'old_value': data1[key],
+                'new_value': data2[key]
+            })       
     return diff
