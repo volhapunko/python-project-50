638f729493c9090d2c1276be0616a587bd4a3b96
Volha <olgapunko1996@gmail.com>
2026-03-22 23:50:14 +0300

    move read_file function

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -2,9 +2,12 @@ from gendiff.builder.build_diff import build_diff
 from gendiff.formatters.json import format_json
 from gendiff.formatters.plain import format_plain
 from gendiff.formatters.stylish import format_stylish
+from gendiff.file_reader import read_file
 
 
-def generate_diff(data1, data2, format_name='stylish'):
+def generate_diff(file1, file2, format_name='stylish'):
+    data1 = read_file(file1)
+    data2 = read_file(file2)
     diff = build_diff(data1, data2)
 
     if format_name == 'stylish':
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -3,96 +3,101 @@ from gendiff.file_reader import read_file
     
 
 def test_generate_diff_json_stylish():
-    data1 = read_file('tests/test_data/file1.json')
-    data2 = read_file('tests/test_data/file2.json')
     expected_result = read_file(
         'tests/test_data/expected_result.txt'
         )
-
-    result = generate_diff(data1, data2)
+    result = generate_diff(
+        'tests/test_data/file1.json',
+        'tests/test_data/file2.json'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_yaml_stylish():
-    data1 = read_file('tests/test_data/file1.yml')
-    data2 = read_file('tests/test_data/file2.yml')
     expected_result = read_file(
         'tests/test_data/expected_result.txt'
         )
-
-    result = generate_diff(data1, data2)
+    result = generate_diff(
+        'tests/test_data/file1.yml',
+        'tests/test_data/file2.yml'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_json_stylish():
-    data1 = read_file('tests/test_data/file3.json')
-    data2 = read_file('tests/test_data/file4.json')
     expected_result = read_file(
         'tests/test_data/expected_nested.txt'
         )
-
-    result = generate_diff(data1, data2)
+    result = generate_diff(
+        'tests/test_data/file3.json',
+        'tests/test_data/file4.json'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_yaml_stylish():
-    data1 = read_file('tests/test_data/file3.yml')
-    data2 = read_file('tests/test_data/file4.yml')
     expected_result = read_file(
         'tests/test_data/expected_nested.txt'
         )
-
-    result = generate_diff(data1, data2)
+    result = generate_diff(
+        'tests/test_data/file3.yml',
+        'tests/test_data/file4.yml'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_json_plain():
-    data1 = read_file('tests/test_data/file3.json')
-    data2 = read_file('tests/test_data/file4.json')
     expected_result = read_file(
         'tests/test_data/expected_plain_nested.txt'
         )
-
-    result = generate_diff(data1, data2, 'plain')
+    result = generate_diff(
+        'tests/test_data/file3.json',
+        'tests/test_data/file4.json',
+        'plain'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_yaml_plain():
-    data1 = read_file('tests/test_data/file3.yml')
-    data2 = read_file('tests/test_data/file4.yml')
     expected_result = read_file(
         'tests/test_data/expected_plain_nested.txt'
         )
-
-    result = generate_diff(data1, data2, 'plain')
+    result = generate_diff(
+        'tests/test_data/file3.yml',
+        'tests/test_data/file4.yml',
+        'plain'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_json_json():
-    data1 = read_file('tests/test_data/file3.json')
-    data2 = read_file('tests/test_data/file4.json')
     expected_result = read_file(
         'tests/test_data/expected_json_nested.txt'
         )
 
-    result = generate_diff(data1, data2, 'json')
+    result = generate_diff(
+        'tests/test_data/file3.json',
+        'tests/test_data/file4.json',
+        'json'
+        )
 
     assert result == expected_result
 
 
 def test_generate_diff_nested_yaml_json():
-    data1 = read_file('tests/test_data/file3.yml')
-    data2 = read_file('tests/test_data/file4.yml')
     expected_result = read_file(
         'tests/test_data/expected_json_nested.txt'
         )
-
-    result = generate_diff(data1, data2, 'json')
+    result = generate_diff(
+        'tests/test_data/file3.yml',
+        'tests/test_data/file4.yml',
+        'json'
+        )
 
     assert result == expected_result
