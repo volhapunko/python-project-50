b2c965fd2f810000f3bf905a253e955243030532
Volha <olgapunko1996@gmail.com>
2026-03-22 23:54:57 +0300

    refactor

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,7 +1,6 @@
 import argparse
 
 from gendiff import generate_diff
-from gendiff.file_reader import read_file
 
 
 def main():
@@ -15,8 +14,5 @@ def main():
 
     args = parser.parse_args()
 
-    data1 = read_file(args.first_file)
-    data2 = read_file(args.second_file)
-
-    diff = generate_diff(data1, data2, args.format)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
